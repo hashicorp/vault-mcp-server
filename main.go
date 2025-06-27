55ffe8c2321325b861d4fdc4a7964841a242e909
Steven Taylor <steven.taylor@hashicorp.com>
2025-06-27 13:53:15 -0700

    Added vault client and sessions + read secret

--- main.go
+++ main.go
@@ -1,35 +1,113 @@
 package main
 
 import (
-    "flag"
-    "github.com/mark3labs/mcp-go/server"
-    "log"
-    "net/http"
-    tools "vault-mcp-server/tools/vault"
+	"context"
+	"flag"
+	"github.com/mark3labs/mcp-go/server"
+	"log"
+	"net/http"
+	"os"
+	"vault-mcp-server/tools"
+	"vault-mcp-server/vault"
 )
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+var VaultAddressHeader string = "VAULT_ADDR"
+var VaultTokenHeader string = "VAULT_TOKEN"
+
+func newSession(ctx context.Context, session server.ClientSession) {
+	// Initialize a new Vault client for this session
+	vaultAddress, ok := ctx.Value(VaultAddressHeader).(string)
+	if !ok {
+		// TODO: Disconnect the session if the token is not provided
+		return
+	}
+
+	vaultToken, ok := ctx.Value(VaultTokenHeader).(string)
+	if !ok {
+		// TODO: Disconnect the session if the token is not provided
+		return
+	}
+
+	_, err := vault.NewVaultClient(session.SessionID(), vaultAddress, vaultToken)
+
+	if err != nil {
+		return
+	}
+	return
+}
+
+func endSession(_ context.Context, session server.ClientSession) {
+	vault.DeleteVaultClient(session.SessionID())
+}
+
+// HeaderMiddleware adds header values to the request context
+func HeaderMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		neededHeaders := [2]string{VaultAddressHeader, VaultTokenHeader}
+
+		ctx := r.Context()
+
+		for _, header := range neededHeaders {
+			// Get the header value from the request
+			headerValue := r.Header.Get(header)
+
+			if headerValue == "" {
+				headerValue = r.URL.Query().Get(header)
+			}
+
+			if headerValue == "" {
+				headerValue = getEnv(header, "")
+			}
+
+			// Create a new context with the header value
+			ctx = context.WithValue(ctx, header, headerValue)
+		}
+
+		// Call the next handler with the new context
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
 func main() {
-    var addr string
-    flag.StringVar(&addr, "addr", ":3000", "address to listen on")
-    flag.Parse()
+	var addr string
 
-    mcpServer := server.NewMCPServer("mcp-server-vault", "0.0.1")
+	flag.StringVar(&addr, "addr", "127.0.0.1:3000", "address to listen on")
+	flag.Parse()
 
-    var serverTools = []server.ServerTool{tools.ReadSecrets()}
+	hooks := &server.Hooks{}
 
-    mcpServer.AddTools(serverTools...)
+	hooks.AddOnRegisterSession(newSession)
+	hooks.AddOnUnregisterSession(endSession)
 
-    handler := server.NewStreamableHTTPServer(
-        mcpServer,
-    )
+	mcpServer := server.NewMCPServer("mcp-server-vault",
+		"0.0.1",
+		server.WithHooks(hooks),
+	)
 
-    mux := http.NewServeMux()
-    mux.Handle("/mcp", handler)
+	var serverTools = []server.ServerTool{tools.ReadSecrets()}
 
-    http.Handle("/streamable-http", handler)
+	mcpServer.AddTools(serverTools...)
 
-    log.Printf("Streamable HTTP server listening on %s", addr)
-    if err := http.ListenAndServe(addr, handler); err != nil {
-        log.Fatalf("Server error: %v", err)
-    }
+	handler := server.NewStreamableHTTPServer(
+		mcpServer,
+		server.WithSessionIdManager(&VaultStatefulSessionIdManager{}),
+	)
+
+	//mux := http.NewServeMux()
+	//mux.Handle("/mcp", HeaderMiddleware(handler))
+
+	http.Handle("/streamable-http", handler)
+
+	log.Printf("Streamable HTTP server listening on %s", addr)
+	if err := http.ListenAndServe(addr, HeaderMiddleware(handler)); err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
 }
---
+++ session-manager.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+    "github.com/google/uuid"
+    "strings"
+)
+
+type VaultStatefulSessionIdManager struct{}
+
+const idPrefix = "mcp-session-"
+
+func (s *VaultStatefulSessionIdManager) Generate() string {
+    sessionId := idPrefix + uuid.New().String()
+    /*
+    	_, err := newVaultClient(sessionId)
+    	if err != nil {
+    		return ""
+    	}
+    */
+    return sessionId
+}
+func (s *VaultStatefulSessionIdManager) Validate(sessionID string) (isTerminated bool, err error) {
+    // validate the session id is a valid uuid
+    if !strings.HasPrefix(sessionID, idPrefix) {
+        return false, fmt.Errorf("invalid session id: %s", sessionID)
+    }
+    if _, err := uuid.Parse(sessionID[len(idPrefix):]); err != nil {
+        return false, fmt.Errorf("invalid session id: %s", sessionID)
+    }
+    return false, nil
+}
+func (s *VaultStatefulSessionIdManager) Terminate(sessionID string) (isNotAllowed bool, err error) {
+    //deleteVaultClient(sessionID)
+    return false, nil
+}
