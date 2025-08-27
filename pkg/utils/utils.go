41c555156d75ee963c20989cc8d20ff419586dda
Gautam <gautambaghel93@gmail.com>
2025-08-26 17:25:38 -0700

    reverting copilot fix, unnecessary

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"net/url"
 	"strings"
 )
 
@@ -20,20 +19,4 @@ func ExtractMountPath(args map[string]any) (string, error) {
 
 func ToBoolPtr(b bool) *bool {
 	return &b
-}
-
-// SanitizeOrigin returns the scheme, hostname, and port from an origin string, or the original string if invalid
-func SanitizeOrigin(origin string) string {
-	parsedURL, err := url.Parse(origin)
-	if err != nil || parsedURL.Scheme == "" || parsedURL.Host == "" {
-		return origin
-	}
-	host := parsedURL.Host
-	// If host does not contain port, but parsedURL.Port() is set, append it
-	if parsedURL.Port() == "" && strings.Contains(host, ":") {
-		// host already contains port
-	} else if parsedURL.Port() != "" && !strings.Contains(host, ":") {
-		host = fmt.Sprintf("%s:%s", parsedURL.Hostname(), parsedURL.Port())
-	}
-	return fmt.Sprintf("%s://%s", parsedURL.Scheme, host)
 }
